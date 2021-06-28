dc8dce379bb7633617a7b685d4ffb5604d468705
Amitha-nair <67319674+Amitha-nair@users.noreply.github.com>
2021-06-28 11:05:42 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>It was a great session that covered all the basics of web-dev</p>
+    </div>
+  );
+}
+export default Info;
